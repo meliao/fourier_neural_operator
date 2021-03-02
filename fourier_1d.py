455ab14fdf1fc6f71c382de99617d5e7e44d6da6
Owen Melia <owenjmelia@gmail.com>
2021-03-01 19:18:27 -0600

    Ready to run fourier_1d.py on TTIC

--- fourier_1d.py
+++ fourier_1d.py
@@ -37,7 +37,7 @@ class SpectralConv1d(nn.Module):
         super(SpectralConv1d, self).__init__()
 
         """
-        1D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        1D Fourier layer. It does FFT, linear transform, and Inverse FFT.
         """
 
         self.in_channels = in_channels
@@ -70,7 +70,7 @@ class SimpleBlock1d(nn.Module):
         2. 4 layers of the integral operators u' = (W + K)(u).
             W defined by self.w; K defined by self.conv .
         3. Project from the channel space to the output space by self.fc1 and self.fc2 .
-        
+
         input: the solution of the initial condition and location (a(x), x)
         input shape: (batchsize, x=s, c=2)
         output: the solution of a later timestep
@@ -146,114 +146,129 @@ class Net1d(nn.Module):
 
         return c
 
+def main(args):
 
-################################################################
-#  configurations
-################################################################
-ntrain = 1000
-ntest = 100
+    ################################################################
+    #  configurations
+    ################################################################
+    ntrain = 1000
+    ntest = 100
 
-sub = 2**3 #subsampling rate
-h = 2**13 // sub #total grid size divided by the subsampling rate
-s = h
+    # sub = 2**3 #subsampling rate
+    # h = 2**13 // sub #total grid size divided by the subsampling rate
+    sub = args.subsample_rate
+    h = args.grid_size // sub
+    s = h
 
-batch_size = 20
-learning_rate = 0.001
+    batch_size = 20
+    learning_rate = 0.001
 
-epochs = 500
-step_size = 100
-gamma = 0.5
+    epochs = args.epochs
+    step_size = 100
+    gamma = 0.5
 
-modes = 16
-width = 64
+    modes = 16
+    width = 64
+
+    ################################################################
+    # read data
+    ################################################################
+
+    # Data is of the shape (number of samples, grid size)
+    dataloader = MatReader(args.data_fp)
+    x_data = dataloader.read_field('a')[:,::sub]
+    y_data = dataloader.read_field('u')[:,::sub]
+
+    x_train = x_data[:ntrain,:]
+    y_train = y_data[:ntrain,:]
+    x_test = x_data[-ntest:,:]
+    y_test = y_data[-ntest:,:]
+
+    # cat the locations information
+    grid = np.linspace(0, 2*np.pi, s).reshape(1, s, 1)
+    grid = torch.tensor(grid, dtype=torch.float)
+    x_train = torch.cat([x_train.reshape(ntrain,s,1), grid.repeat(ntrain,1,1)], dim=2)
+    x_test = torch.cat([x_test.reshape(ntest,s,1), grid.repeat(ntest,1,1)], dim=2)
+
+    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), batch_size=batch_size, shuffle=False)
+
+    # model
+    model = Net1d(modes, width).cuda()
+    print("Number of model parameters:", model.count_params())
 
 
-################################################################
-# read data
-################################################################
+    ################################################################
+    # training and evaluation
+    ################################################################
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 
-# Data is of the shape (number of samples, grid size)
-dataloader = MatReader('data/burgers_data_R10.mat')
-x_data = dataloader.read_field('a')[:,::sub]
-y_data = dataloader.read_field('u')[:,::sub]
+    myloss = LpLoss(size_average=False)
+    for ep in range(epochs):
+        model.train()
+        t1 = default_timer()
+        train_mse = 0
+        train_l2 = 0
+        for x, y in train_loader:
+            x, y = x.cuda(), y.cuda()
 
-x_train = x_data[:ntrain,:]
-y_train = y_data[:ntrain,:]
-x_test = x_data[-ntest:,:]
-y_test = y_data[-ntest:,:]
+            optimizer.zero_grad()
+            out = model(x)
 
-# cat the locations information
-grid = np.linspace(0, 2*np.pi, s).reshape(1, s, 1)
-grid = torch.tensor(grid, dtype=torch.float)
-x_train = torch.cat([x_train.reshape(ntrain,s,1), grid.repeat(ntrain,1,1)], dim=2)
-x_test = torch.cat([x_test.reshape(ntest,s,1), grid.repeat(ntest,1,1)], dim=2)
+            mse = F.mse_loss(out, y, reduction='mean')
+            # mse.backward()
+            l2 = myloss(out.view(batch_size, -1), y.view(batch_size, -1))
+            l2.backward() # use the l2 relative loss
 
-train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), batch_size=batch_size, shuffle=False)
+            optimizer.step()
+            train_mse += mse.item()
+            train_l2 += l2.item()
 
-# model
-model = Net1d(modes, width).cuda()
-print(model.count_params())
+        scheduler.step()
+        model.eval()
+        test_l2 = 0.0
+        with torch.no_grad():
+            for x, y in test_loader:
+                x, y = x.cuda(), y.cuda()
 
+                out = model(x)
+                test_l2 += myloss(out.view(batch_size, -1), y.view(batch_size, -1)).item()
 
-################################################################
-# training and evaluation
-################################################################
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+        train_mse /= len(train_loader)
+        train_l2 /= ntrain
+        test_l2 /= ntest
 
-myloss = LpLoss(size_average=False)
-for ep in range(epochs):
-    model.train()
-    t1 = default_timer()
-    train_mse = 0
-    train_l2 = 0
-    for x, y in train_loader:
-        x, y = x.cuda(), y.cuda()
+        t2 = default_timer()
+        print(ep, t2-t1, train_mse, train_l2, test_l2)
 
-        optimizer.zero_grad()
-        out = model(x)
-
-        mse = F.mse_loss(out, y, reduction='mean')
-        # mse.backward()
-        l2 = myloss(out.view(batch_size, -1), y.view(batch_size, -1))
-        l2.backward() # use the l2 relative loss
-
-        optimizer.step()
-        train_mse += mse.item()
-        train_l2 += l2.item()
-
-    scheduler.step()
-    model.eval()
-    test_l2 = 0.0
+    torch.save(model, args.model_fp)
+    pred = torch.zeros(y_test.shape)
+    index = 0
+    test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), batch_size=1, shuffle=False)
     with torch.no_grad():
         for x, y in test_loader:
+            test_l2 = 0
             x, y = x.cuda(), y.cuda()
 
             out = model(x)
-            test_l2 += myloss(out.view(batch_size, -1), y.view(batch_size, -1)).item()
+            pred[index] = out
 
-    train_mse /= len(train_loader)
-    train_l2 /= ntrain
-    test_l2 /= ntest
+            test_l2 += myloss(out.view(1, -1), y.view(1, -1)).item()
+            print(index, test_l2)
+            index = index + 1
 
-    t2 = default_timer()
-    print(ep, t2-t1, train_mse, train_l2, test_l2)
+    scipy.io.savemat(args.preds_fp, mdict={'pred': pred.cpu().numpy()})
 
-# torch.save(model, 'model/ns_fourier_burgers_8192')
-pred = torch.zeros(y_test.shape)
-index = 0
-test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), batch_size=1, shuffle=False)
-with torch.no_grad():
-    for x, y in test_loader:
-        test_l2 = 0
-        x, y = x.cuda(), y.cuda()
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_fp')
+    parser.add_argument('--model_fp')
+    parser.add_argument('--preds_fp')
+    parser.add_argument('--subsample_rate', type=int, default=2**3)
+    parser.add_argument('--grid_size', type=int, default=2**13)
+    parser.add_argument('--epochs', type=int, default=500)
 
-        out = model(x)
-        pred[index] = out
-
-        test_l2 += myloss(out.view(1, -1), y.view(1, -1)).item()
-        print(index, test_l2)
-        index = index + 1
-
-# scipy.io.savemat('pred/burger_test.mat', mdict={'pred': pred.cpu().numpy()})
+    args = parser.parse_args()
+    main(args)
