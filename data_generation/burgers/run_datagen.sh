2fc8a997e6345fb89ca8948b47d23a70bd3faffa
Owen Melia <meliao@cs.uchicago.edu>
2021-03-08 10:31:54 -0600

    Changes from cs linux

--- data_generation/burgers/run_datagen.sh
+++ data_generation/burgers/run_datagen.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name=gen_data_N_256_s_512
 #SBATCH --partition=general
-#SBATCH --output=/gen_data_N_256_s_512.out
-#SBATCH --error={{ logs_folder }}/gen_data_N_256_s_512.err
+#SBATCH --output=/home/meliao/projects/fourier_neural_operator/data_generation/burgers/gen_data_N_256_s_512.out
+#SBATCH --error=/home/meliao/projects/fourier_neural_operator/data_generation/burgers/gen_data_N_256_s_512.err
 echo "`date` Starting Job"
 echo "SLURM Info: Job name:${SLURM_JOB_NAME}"
 echo "    JOB ID: ${SLURM_JOB_ID}"
