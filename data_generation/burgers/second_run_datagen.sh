3aec77ab81c34c8cae7b9525be7dfe4bbc594d86
Owen Melia <meliao@cs.uchicago.edu>
2021-03-17 09:31:04 -0500

    Burgers data generation changes from cs-linux

--- data_generation/burgers/second_run_datagen.sh
+++ data_generation/burgers/second_run_datagen.sh
@@ -2,6 +2,7 @@
 
 #SBATCH --job-name=gen_data_N_256_s_512
 #SBATCH --partition=general
+#SBATCH --mem=8000M
 #SBATCH --output=/home/meliao/projects/fourier_neural_operator/data_generation/burgers/01_gen_data.out
 #SBATCH --error=/home/meliao/projects/fourier_neural_operator/data_generation/burgers/01_gen_data.err
 echo "`date` Starting Job"
@@ -11,4 +12,4 @@ echo "    Host list: ${SLURM_JOB_NODELIST}"
 
 matlab \
  -batch \
-"N=100;seed=0;s=8192;out_fp='~/projects/fourier_neural_operator/data/01_test_Burgers.mat';gen_burgers1;"
+"N=5;seed=0;s=8192;out_fp='~/projects/fourier_neural_operator/data/01_test_Burgers.mat';gen_burgers1;"
