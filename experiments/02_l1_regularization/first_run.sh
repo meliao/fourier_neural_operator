c2408ffe8bb4aa3ff437c12e208440c9736ffd37
Owen Melia <owenjmelia@gmail.com>
2021-03-31 20:50:10 -0500

    Adding l1_reg experiment

---
+++ experiments/02_l1_regularization/first_run.sh
@@ -0,0 +1,15 @@
+for i in plots preds models results jobs logs
+do
+  if [[ ! -d $i ]]; then
+    mkdir $i
+  fi
+done
+
+python fourier_1d.py \
+--data_fp ~/projects/fourier_neural_operator/data/2021-03-17_training_Burgers_data_GRF1.mat \
+--model_fp models/first_run \
+--preds_fp preds/first_run.mat \
+--results_fp results/first_run.txt \
+--epochs 3 \
+--freq_modes 4 \
+--l1_lambda 10
