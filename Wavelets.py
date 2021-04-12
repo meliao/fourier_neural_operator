d027b643df3925f6f0cc354629efac4577328686
Owen Melia <owenjmelia@gmail.com>
2021-04-12 17:26:58 -0500

    Updated Wavelets code

--- Wavelets.py
+++ Wavelets.py
@@ -4,63 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class IHaarDWT(nn.Module):
-    """Short summary.
-
-    Attributes
-    ----------
-    c_filter : pytorch Tensor
-        Filter to produce c_i coefficients in DWT. Sometimes referred to as h_0.
-    d_filter : type
-        Filter to produce d_i coefficients in DWT. Sometimes referred to as h_1.
-    padder : pytorch nn Module
-        Appropriately pads odd-length arrays
-    level : int
-        Level of the DWT
-    """
-    def __init__(self, level=1):
-        super().__init__()
-        self.c_filter = torch.tensor(np.divide(np.array([1., 1.]),
-                                                np.sqrt(2)), dtype=torch.float).reshape((2,1))
-        self.d_filter = torch.tensor(np.divide(np.array([1., -1.]),
-                                                np.sqrt(2)), dtype=torch.float).reshape((2,1))
-        self.filter_len = 2
-        self.level = level
-
-    def max_dwt_level(self, data_len):
-        """
-        This is a function to compute the maximum level DWT that is possible
-        on a 1D input of length data_len. This formula is copied from
-        PyWavelets: https://tinyurl.com/y9u7yvbw
-        """
-        return np.floor(np.log2(data_len / (self.filter_len - 1)))
-
-    def unfilter(self, x):
-        xlen = x.size()[-1]
-        x = x.reshape((2, int(xlen / 2))).transpose(0,1)
-        c_out = torch.mm(x, self.c_filter)
-        d_out = torch.mm(x, self.d_filter)
-
-        out = torch.hstack((c_out, d_out)).flatten()
-        return out
-
-    def forward(self, x, verbose=False):
-        # x has shape (1,1,xlen). We need to transform it into (2,xlen / 2)
-        xlen = x.size()[-1]
-
-        if xlen % 2:
-            raise ValueError("Expected even-length input but recieved length {}".format(xlen))
-        for l in range(self.level-1, -1, -1):
-            level_i_arr_idx = int(xlen / (2 ** l))
-            x_in = x[:,:,:level_i_arr_idx]
-            if verbose:
-                print("Level {} x_in: {}".format(l, x_in))
-            x[:,:,:level_i_arr_idx] = self.unfilter(x_in)
-            if verbose:
-                print("Level {} x_out: {}".format(l, x))
-
-        return x
-
 class HaarDWT(nn.Module):
     """Short summary.
 
@@ -75,14 +18,34 @@ class HaarDWT(nn.Module):
     level : int
         Level of the DWT
     """
-    def __init__(self, level=1):
+    def __init__(self, width, input_len, level=None, device=None):
         super().__init__()
+
+        self.filter_len = 2
+
+        if level is not None:
+            self.level = level
+        else:
+            self.level = self.max_dwt_level(input_len)
+        self.input_len = input_len
+        self.width = width
+
+        # This is bit manipulation to assert that xlen is a power of 2
+        if not ((self.input_len & (self.input_len - 1) == 0) and self.input_len != 0):
+            raise ValueError("Input array length {} is not power of 2".format(self.input_len))
+        if self.level > self.max_dwt_level(self.input_len):
+            s = "Input array length {} gives max DWT level {}".format(self.input_len,
+                                                                      self.max_dwt_level(self.input_len))
+            raise ValueError(s)
+
+
         self.c_filter = torch.tensor(np.divide(np.array([1., 1.]),
                                                 np.sqrt(2)), dtype=torch.float).reshape((1,1,2))
         self.d_filter = torch.tensor(np.divide(np.array([1., -1.]),
                                                 np.sqrt(2)), dtype=torch.float).reshape((1,1,2))
-        self.filter_len = 2
-        self.level = level
+        if device is not None:
+            self.c_filter = self.c_filter.to(device)
+            self.d_filter = self.d_filter.to(device)
 
         # If the input array is odd-length, pad the array by repeating the
         # last element
@@ -94,30 +57,201 @@ class HaarDWT(nn.Module):
         on a 1D input of length data_len. This formula is copied from
         PyWavelets: https://tinyurl.com/y9u7yvbw
         """
-        return np.floor(np.log2(data_len / (self.filter_len - 1)))
+        return int(np.floor(np.log2(data_len / (self.filter_len - 1))))
 
     def filter(self, x):
-        # To get odd-length arrays matching pywt output, we need to repeat the
-        # last element of the array
-        if x.shape[-1] % 2:
-            x = self.padder(x)
+        xlen = x.size()[-1]
+
+
+        # We want the filter to work independently on each element of the batch
+        # and each width channel.
+        x = x.reshape((-1, 1, xlen))
 
         c_out = F.conv1d(x, self.c_filter, stride=2)
         d_out = F.conv1d(x, self.d_filter, stride=2)
         out = torch.cat((c_out, d_out), axis=-1)
+        out = out.reshape((-1, self.width, xlen))
         return out
 
 
     def forward(self, x):
-        xlen = x.size()[-1]
+        """
+        Expects input of size (batch_n, width, input_len) where
+            batch_num unconstrained and xlen is a power of 2
+        """
+
+        for l in range(self.level):
+            level_i_arr_idx = int(self.input_len / (2 ** l))
+            x_out = x.clone()
+            x_in = x[:,:,:level_i_arr_idx]
+            x_out[:,:,:level_i_arr_idx] = self.filter(x_in)
+            x = x_out
+        return x
+
+
+class IHaarDWT(nn.Module):
+    """Short summary.
+
+    Parameters
+    ----------
+    width : type
+        Description of parameter `width`.
+    input_len : type
+        Description of parameter `input_len`.
+    level : type
+        Description of parameter `level`.
+    device : type
+        Description of parameter `device`.
+
+    Attributes
+    ----------
+    filter_len : type
+        Description of attribute `filter_len`.
+    max_dwt_level : type
+        Description of attribute `max_dwt_level`.
+    c_filter : type
+        Description of attribute `c_filter`.
+    d_filter : type
+        Description of attribute `d_filter`.
+    level
+    input_len
+    width
+
+    """
+    def __init__(self, width, input_len, level=None, device=None):
+        super().__init__()
+
+        self.filter_len = 2
+
+        if level is not None:
+            self.level = level
+        else:
+            self.level = self.max_dwt_level(input_len)
+        self.input_len = input_len
+        self.width = width
 
         # This is bit manipulation to assert that xlen is a power of 2
-        if not ((xlen & (xlen-1) == 0) and xlen != 0):
-            raise ValueError("Input array length {} is not power of 2".format(xlen))
-        if self.level > self.max_dwt_level(xlen):
-            raise ValueError("Input array length {} gives max DWT level {}".format(xlen, self.max_dwt_level(xlen)))
-        for l in range(self.level):
-            level_i_arr_idx = int(xlen / (2 ** l))
+        if not ((self.input_len & (self.input_len - 1) == 0) and self.input_len != 0):
+            raise ValueError("Input array length {} is not power of 2".format(self.input_len))
+        if self.level > self.max_dwt_level(self.input_len):
+            s = "Input array length {} gives max IDWT level {}".format(self.input_len,
+                                                                      self.max_dwt_level(self.input_len))
+            raise ValueError(s)
+        self.c_filter = torch.tensor(np.divide(np.array([1., 1.]),
+                                                np.sqrt(2)), dtype=torch.float).reshape((1,2,1))
+        self.d_filter = torch.tensor(np.divide(np.array([1., -1.]),
+                                                np.sqrt(2)), dtype=torch.float).reshape((1,2,1))
+        if device is not None:
+            self.c_filter = self.c_filter.to(device)
+            self.d_filter = self.d_filter.to(device)
+
+
+    def max_dwt_level(self, data_len):
+        """
+        This is a function to compute the maximum level DWT that is possible
+        on a 1D input of length data_len. This formula is copied from
+        PyWavelets: https://tinyurl.com/y9u7yvbw
+        """
+        return int(np.floor(np.log2(data_len / (self.filter_len - 1))))
+
+    def unfilter(self, x):
+        """Perform a Level-1 Inverse Haar DWT via filter banks. Performs this
+        transformation along the last axis. The elements of the first 2 axes are
+        treated completely independently
+
+        Parameters
+        ----------
+        x : pytorch Tensor
+            Assumed to be of size (batch_size, self.width, xlen) where xlen is
+            a power of 2
+
+        Returns
+        -------
+        pytorch Tensor
+            output size (batch_size, self.width, xlen).
+
+        """
+
+        # X has shape (batch_num, self.width, xlen)
+        xlen = x.size()[-1]
+        batch_num = x.size()[0]
+
+        # We want X to have shape (batch_num, self.width, 2, xlen/2)
+        x = x.reshape((batch_num, self.width, 2, int(xlen/2))) # .permute(0,1,3,2)
+
+        # The IDWT works independently on each element along the batch and
+        # width axes, so we're combining those two.
+        # x now has shape (batch_num * self.width, 2, xlen / 2)
+        x = x.flatten(start_dim=0, end_dim=1)
+        c_out = F.conv1d(x, self.c_filter)
+        d_out = F.conv1d(x, self.d_filter)
+
+        # This code is messy, but in essence, it takes c_out and d_out, each
+        # with shapes (batch_num * self.width, 2, xlen / 2), and interleaves
+        # along the last axis. So the output array has shape (batch_num * self.width, 1, xlen)
+        out = torch.cat((c_out, d_out), dim=1).permute(0,2,1).flatten(start_dim=1)
+
+        # This undoes the combining of the batch and width axes. Output shape
+        # now is (batch_num, self.width, xlen) == input shape.
+        out = out.reshape((batch_num, self.width, xlen))
+        return out
+
+    def forward(self, x):
+        """Perform a Inverse Haar DWT via filter banks. Performs this
+        transformation along the last axis. The elements of the first 2 axes are
+        treated completely independently. The level of the IDWT is determined
+        by self.level
+
+        Parameters
+        ----------
+        x : pytorch Tensor
+            Assumed to be of size (batch_size, self.width, self.input_len)
+
+        Returns
+        -------
+        pytorch Tensor
+            output size (batch_size, self.width, self.input_len).
+
+        """
+        for l in range(self.level-1, -1, -1):
+            level_i_arr_idx = int(self.input_len / (2 ** l))
+            x_out = x.clone()
             x_in = x[:,:,:level_i_arr_idx]
-            x[:,:,:level_i_arr_idx] = self.filter(x_in)
+            x_out[:,:,:level_i_arr_idx] = self.unfilter(x_in)
+            x = x_out
         return x
+
+
+class WaveletBlock1d(nn.Module):
+    def __init__(self, width, input_len, keep, device=None):
+        super(WaveletBlock1d, self).__init__()
+        self.input_len = input_len
+        self.width = width
+        self.keep = keep
+
+        self.DWT = HaarDWT(width=width, input_len=input_len, device=device)
+        self.IDWT = IHaarDWT(width=width, input_len=input_len, device=device)
+
+        self.scale = (1 / (self.width**2))
+        self.weights = nn.Parameter(self.scale * torch.rand(self.width,
+                                                            self.width,
+                                                            self.keep))
+
+    def forward(self, x):
+        # x has shape (batch_size, width, input_len)
+        # Do DWT row-by-row
+        z = torch.zeros(x.size(), device=x.device)
+
+        x = self.DWT(x)
+
+
+        # We want to keep only the specified number of coefficients.
+        # The high-DWT-level coefficients are towards the left of the array.
+        z[:,:,:self.keep] = x[:,:,:self.keep]
+
+
+        # ok so now z has the DWT coefficients. z is of shape (batch_size, width, input_len)
+        z = torch.einsum('bix,iox->box', z, self.weights)
+
+        out = self.IDWT(z)
+        return out
--- test/testWavelets.py
+++ test/testWavelets.py
@@ -8,143 +8,127 @@ import Wavelets
 
 class TestHaarDWT(unittest.TestCase):
     def setUp(self):
-        # d_arrays = deterministic arrays. As opposed to tests on random data
-        self.d_arrays = {}
-        self.d_arrays['000_ones'] = np.ones(128)
-        self.d_arrays['001_ones'] = np.ones(256)
-        self.d_arrays['002_zeros'] = np.zeros(128)
-        self.d_arrays['003_zeros'] = np.zeros(256)
-        self.d_arrays['004_linspace'] = np.linspace(0, 10, 512)
-        self.d_arrays['005_linspace'] = np.linspace(0, 10, 1024)
-        self.d_arrays['006_sin'] = np.sin(np.linspace(0, 10, 512))
-        self.d_arrays['007_cos'] = np.cos(np.linspace(0, 10, 1024))
-        self.d_arrays['008_abs'] = np.abs(np.linspace(-33, 33, 1024))
+        # a_arrays = tested against pywt reference for DWT and IDWT
+        self.a_arrays = {}
+        self.a_arrays['000_ones'] = np.ones((1,1,128))
+        self.a_arrays['001_ones'] = np.ones((1,1,256))
+        self.a_arrays['002_zeros'] = np.zeros((1,1,128))
+        self.a_arrays['003_zeros'] = np.zeros((1,1,256))
+        self.a_arrays['004_linspace'] = np.linspace(0, 10, 512).reshape((1,1,512))
+        self.a_arrays['005_linspace'] = np.linspace(0, 10, 1024).reshape((1,1,1024))
+        self.a_arrays['006_sin'] = np.sin(np.linspace(0, 10, 512)).reshape((1,1,512))
+        self.a_arrays['007_cos'] = np.cos(np.linspace(0, 10, 1024)).reshape((1,1,1024))
+        self.a_arrays['008_abs'] = np.abs(np.linspace(-33, 33, 1024)).reshape((1,1,1024))
 
-        # r_arrays = random data arrays
-        self.r_arrays = {}
-        self.r_arrays['009_runif'] = np.random.uniform(size=1024)
-        self.r_arrays['010_rnorm'] = np.random.normal(size=1024)
+        # b_arrays = random data arrays
+        self.b_arrays = {}
+        self.b_arrays['009_runif'] = np.random.uniform(size=10240).reshape((2,5,1024))
+        self.b_arrays['010_runif'] = np.random.uniform(size=5120).reshape((5,2,512))
+        self.b_arrays['011_runif'] = np.random.uniform(size=11520).reshape((9,10,128))
+        self.b_arrays['010_rnorm'] = np.random.normal(size=10240).reshape((10,1,1024))
 
 
     @staticmethod
     def torch_to_numpy(ten):
-        s = ten.size()[-1]
-        return ten.numpy().reshape(s)
+        return ten.numpy()
 
     @staticmethod
     def numpy_to_torch(arr):
-        s = arr.shape[-1]
-        return torch.tensor(arr).reshape((1,1,s))
+        return torch.from_numpy(arr).float()
 
     @staticmethod
     def pywt_reference_HaarDWT_level1(arr):
+        original_arr_shape = arr.shape
+        arr = arr.flatten()
         c,d = pywt.dwt(arr, 'haar')
-        return np.concatenate((c,d))
+        return np.concatenate((c,d)).reshape(original_arr_shape)
 
     @staticmethod
     def pywt_reference_HaarDWT_leveln(arr, n, verbose=False):
+        original_arr_shape = arr.shape
+        arr = arr.flatten()
         coeffs = pywt.wavedec(arr, 'haar', level=n)
-        return np.concatenate(coeffs)
+        return np.concatenate(coeffs).reshape(original_arr_shape)
 
     @staticmethod
     def pywt_reference_IHaarDWT_level1(arr):
+        original_arr_shape = arr.shape
+        arr = arr.flatten()
         l = arr.shape[-1]
         c = arr[:int(l/2)]
         d = arr[int(l/2):]
-        out = pywt.idwt(c, d, 'haar')
+        out = pywt.idwt(c, d, 'haar').reshape(original_arr_shape)
         return out
 
+    def assert_close_and_show_diff(self, ref, ans):
+        errors = ref - ans
+        errors = errors.flatten()
+        l_inf = np.linalg.norm(errors, ord=np.inf)
+        l_2 = np.linalg.norm(errors, ord=2)
+        s = "Errors: L_2 = {:.03e} L_inf = {:.03e}".format(l_2, l_inf)
+        self.assertTrue(np.allclose(ref, ans, atol=5e-06), s)
+
+    def get_ref_ans_DWT(self, arr, level=1):
+        DWT_obj = Wavelets.HaarDWT(arr.shape[1], arr.shape[2], level=level)
+        dwt_ref = self.pywt_reference_HaarDWT_leveln(arr, n=level)
+        arr_ten = self.numpy_to_torch(arr)
+        dwt_ans_ten = DWT_obj(arr_ten)
+        dwt_ans = self.torch_to_numpy(dwt_ans_ten)
+        return dwt_ref, dwt_ans
+
+    def get_ref_ans_IDWT(self, arr):
+        IDWT_obj = Wavelets.IHaarDWT(arr.shape[1], arr.shape[2], level=1)
+        dwt_ref = self.pywt_reference_HaarDWT_level1(arr)
+        idwt_ref = self.pywt_reference_IHaarDWT_level1(dwt_ref)
+        dwt_ten = self.numpy_to_torch(dwt_ref)
+        idwt_ans_ten = IDWT_obj(dwt_ten)
+        idwt_ans = self.torch_to_numpy(idwt_ans_ten)
+
+        return idwt_ref, idwt_ans
+
+    def get_DWT_IDWT_identity(self, arr):
+        DWT_obj = Wavelets.HaarDWT(arr.shape[1], arr.shape[2])
+        IDWT_obj = Wavelets.IHaarDWT(arr.shape[1], arr.shape[2])
+        arr_ten = self.numpy_to_torch(arr)
+        dwt_ten = DWT_obj(arr_ten)
+        tensor_print(dwt_ten, "DWT_TEN")
+        idwt_ten = IDWT_obj(dwt_ten)
+        return self.torch_to_numpy(idwt_ten)
+
+
     def test_DWTinit(self):
-        DWT_obj = Wavelets.HaarDWT()
+        DWT_obj = Wavelets.HaarDWT(width=4, input_len=4)
         self.assertIsInstance(DWT_obj, Wavelets.HaarDWT)
 
     def test_IDWTinit(self):
-        IDWT_obj = Wavelets.IHaarDWT()
+        IDWT_obj = Wavelets.IHaarDWT(width=4, input_len=4)
         self.assertIsInstance(IDWT_obj, Wavelets.IHaarDWT)
 
     def test_bad_input_length_DWT(self):
-        DWT_obj = Wavelets.HaarDWT()
-        x = torch.zeros((1,1,1+1024))
         with self.assertRaises(ValueError):
-            y = DWT_obj(x)
-    
+            DWT_obj = Wavelets.HaarDWT(1, 1+1024)
 
-    def testHaarDWTLevel1_deterministic(self):
-        DWT_obj = Wavelets.HaarDWT()
-        for k, v in self.d_arrays.items():
-            with self.subTest(k=k):
-                v_ten = self.numpy_to_torch(v)
-                ans = self.torch_to_numpy(DWT_obj(v_ten))
-                ref = self.pywt_reference_HaarDWT_level1(v)
-                self.assertTrue(np.allclose(ref, ans), k)
-
-    def testHaarDWTLeveln_deterministic(self):
-        for k, v in self.d_arrays.items():
+    def test_dwt_against_reference(self):
+        for k, v in self.a_arrays.items():
             for level in [1,2,3,4]:
                 with self.subTest(k=k, level=level):
-                    DWT_obj = Wavelets.HaarDWT(level=level)
-                    v_ten = self.numpy_to_torch(v)
-                    ans = self.torch_to_numpy(DWT_obj(v_ten))
-                    ref = self.pywt_reference_HaarDWT_leveln(v, n=level)
-                    self.assertTrue(np.allclose(ref, ans), k)
+                    dwt_ref, dwt_ans = self.get_ref_ans_DWT(v, level=level)
+                    self.assert_close_and_show_diff(dwt_ref, dwt_ans)
 
-    def testHaarDWTLeveln_random(self):
-        for k, v in self.r_arrays.items():
-            for level in [1,2,3,4]:
+    def test_idwt_against_reference(self):
+        for k, v in self.a_arrays.items():
+            with self.subTest(k=k):
+                idwt_ref, idwt_ans = self.get_ref_ans_IDWT(v)
+                self.assert_close_and_show_diff(idwt_ref, idwt_ans)
+
+    def test_invertibility(self):
+        for k, v in self.b_arrays.items():
+            for level in [1,2,3,4,None]:
                 with self.subTest(k=k, level=level):
-                    DWT_obj = Wavelets.HaarDWT(level=level)
+                    DWT_obj = Wavelets.HaarDWT(v.shape[1], v.shape[2], level=level)
+                    IDWT_obj = Wavelets.IHaarDWT(v.shape[1], v.shape[2], level=level)
                     v_ten = self.numpy_to_torch(v)
-                    ans = self.torch_to_numpy(DWT_obj(v_ten))
-                    ref = self.pywt_reference_HaarDWT_leveln(v, n=level)
-                    self.assertTrue(np.allclose(ref, ans), k)
-
-    def testHaarDWTLevel1_random(self):
-        DWT_obj = Wavelets.HaarDWT()
-        for k, v in self.r_arrays.items():
-            with self.subTest(k=k):
-                v_ten = self.numpy_to_torch(v)
-                ans = self.torch_to_numpy(DWT_obj(v_ten))
-                ref = self.pywt_reference_HaarDWT_level1(v)
-                self.assertTrue(np.allclose(ref, ans), k)
-
-    def testIHaarDWTLevel1_deterministic(self):
-        IDWT_obj = Wavelets.IHaarDWT()
-        for k, v in self.d_arrays.items():
-            with self.subTest(k=k):
-                dwt = self.pywt_reference_HaarDWT_level1(v)
-                dwt_ten = self.numpy_to_torch(dwt)
-                ans = self.torch_to_numpy(IDWT_obj(dwt_ten))
-                ref = self.pywt_reference_IHaarDWT_level1(dwt)
-                self.assertTrue(np.allclose(ref, ans), k)
-
-    def testIHaarDWTLevel1_random(self):
-        IDWT_obj = Wavelets.IHaarDWT()
-        for k, v in self.r_arrays.items():
-            with self.subTest(k=k):
-                dwt = self.pywt_reference_HaarDWT_level1(v)
-                dwt_ten = self.numpy_to_torch(dwt)
-                ans = self.torch_to_numpy(IDWT_obj(dwt_ten))
-                ref = self.pywt_reference_IHaarDWT_level1(dwt)
-                self.assertTrue(np.allclose(ref, ans), k)
-
-    def testIHaarDWTLeveln_deterministic(self):
-        for k, v in self.d_arrays.items():
-            for level in [1,2,3,4]:
-                with self.subTest(k=k, level=level):
-                    v_ten = self.numpy_to_torch(v)
-                    DWT_obj = Wavelets.HaarDWT(level=level)
-                    IDWT_obj = Wavelets.IHaarDWT(level=level)
                     dwt_ten = DWT_obj(v_ten)
-                    ans = self.torch_to_numpy(IDWT_obj(dwt_ten))
-                    self.assertTrue(np.allclose(v, ans), k)
-
-    def testIHaarDWTLeveln_random(self):
-        for k, v in self.r_arrays.items():
-            for level in [1,2,3,4]:
-                with self.subTest(k=k, level=level):
-                    v_ten = self.numpy_to_torch(v)
-                    DWT_obj = Wavelets.HaarDWT(level=level)
-                    IDWT_obj = Wavelets.IHaarDWT(level=level)
-                    dwt_ten = DWT_obj(v_ten)
-                    ans = self.torch_to_numpy(IDWT_obj(dwt_ten))
-                    self.assertTrue(np.allclose(v, ans), k)
+                    idwt_ten = IDWT_obj(dwt_ten)
+                    idwt = self.torch_to_numpy(idwt_ten)
+                    self.assert_close_and_show_diff(idwt, v)
