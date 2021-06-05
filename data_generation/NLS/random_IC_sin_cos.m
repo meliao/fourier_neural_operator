7877c8d8d0806122569cdee37b6099aa6192080b
Owen Melia <owenjmelia@gmail.com>
2021-06-05 16:58:57 -0500

    Changes to random IC creation

--- data_generation/NLS/random_IC_sin_cos.m
+++ data_generation/NLS/random_IC_sin_cos.m
@@ -11,7 +11,7 @@ function out_cfun = random_IC_sin_cos(X_bounds, n_fourier_coef)
     for i = 1:n_fourier_coef
         draw = rdata(i,:);
         k = i - 1;
-        new_func = @(x)  draw(1) * sin(k * pi * x + draw(2)) + draw(3) * cos(k * pi * x + draw(4));
+        new_func = @(x)  draw(1) * sin(k * x + draw(2)) + draw(3) * cos(k * x + draw(4));
         ic_func = @(x) ic_func(x) + new_func(x);
     end
     ic_vals = ic_func(linspace(X_bounds(1), X_bounds(2), 1024));
