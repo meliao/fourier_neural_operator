ad8c9855517786fbd1cc3188b528693f16dd95f7
meliao <owenjmelia@gmail.com>
2021-07-05 16:16:06 -0500

    Changes from ttic

--- experiments/09_predict_residuals/first_train_models.sh
+++ experiments/09_predict_residuals/first_train_models.sh
@@ -18,8 +18,8 @@ cd ~/projects/fourier_neural_operator/
 conda activate fourier_neural_operator
 
 python -m experiments.09_predict_residuals.train_models \
---data_fp data/2021-06-24_NLS_data_04_train.mat \
---test_data_fp data/2021-06-24_NLS_data_04_test.mat \
+--data_fp /share/data/willett-group/meliao/data/2021-06-24_NLS_data_04_train.mat \
+--test_data_fp /share/data/willett-group/meliao/data/2021-06-24_NLS_data_04_test.mat \
 --emulator_fp experiments/08_FNO_pretraining/models/00_pretrain_ep_1000 \
 --model_fp experiments/09_predict_residuals/models/00_residual_ep_{} \
 --train_df experiments/09_predict_residuals/results/00_residual_train.txt \
